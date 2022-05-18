eb1afca48781a4aafbed3f0e8ac56635f88d701d
josefl <jose.fl_@hotmail.com>
2022-05-18 10:01:28 -0500

    agregando metodo get by ID y metodo POST

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 // HTTP METHODS
 app.get('/v1/explorers', (req, res) => {
-    console.log(`ÀPI Explorers GET ALL request ${new Date()}`)
+    console.log(`API Explorers GET ALL request ${new Date()}`)
     const explorer1 = {id: 1, name: "Jose"}
     const explorer2 = {id: 2, name: "Javier"}
     const explorer3 = {id: 3, name: "Memo"}
@@ -19,7 +19,20 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 });
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`API Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: "Jose"}
+    res.status(200).json(explorer)
+});
+
+app.post('/v1/explorers', (req, res) => {
+    console.log(`API Explorers POST Request ${new Date()}`)
+    const requestBody = req.body        // Parametros de un cliente
+    res.status(201).json({message: "Created"});
+});
+
 // Inicializacion de la app:
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
