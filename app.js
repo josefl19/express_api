e8343164992e157ec928b3339c0dbf3f20805459
josefl <jose.fl_@hotmail.com>
2022-05-18 09:40:14 -0500

    creacion de un método GET con lista de explorers

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(express.json())     // Indicamos el uso de JSON
 // Puerto de la app
 const port = 3000;
 
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`ÀPI Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id: 1, name: "Jose"}
+    const explorer2 = {id: 2, name: "Javier"}
+    const explorer3 = {id: 3, name: "Memo"}
+    const explorer4 = {id: 4, name: "Jessamyn"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4];
+    res.status(200).json(explorers)
+});
+
 // Inicializacion de la app:
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
